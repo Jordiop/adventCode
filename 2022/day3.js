25775ec1cd7c3c96c31f5617c1befca8da360819
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2022-12-30 18:51:59 +0100

    Day 3 completed

--- 2022/day3.js
+++ 2022/day3.js
@@ -13,7 +13,7 @@ let alphabet = ['_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 
 
 
-
+// Searching repeated letters
 
 for (let i = 0; i < sacks.length; i++) {
     let temp = sacks[i].slice(0, sacks[i].length / 2);
@@ -30,14 +30,34 @@ for (let i = 0; i < sacks.length; i++) {
 
 repetidos.filter(letra => letra != undefined);
 
-let valor = 0;
-let sum = 0;
-let letraTemp;
 
-for (let k = 0; k < repetidos.length; k++) {
-    letraTemp = repetidos[k];
-    valor = alphabet.indexOf(letraTemp);
-    sum += valor;
+function contar(array) {
+    let valor = 0;
+    let sum = 0;
+    let letraTemp;
+    for (let k = 0; k < array.length; k++) {
+        letraTemp = array[k];
+        valor = alphabet.indexOf(letraTemp);
+        sum += valor;
+    }
+    return sum;
 }
 
-console.log(sum);
+let array2 = [];
+
+for (let a = 0; a < sacks.length; a += 3) {
+    let word1 = sacks[a];
+    let word2 = sacks[a + 1];
+    let word3 = sacks[a + 2];
+    let comprobante1 = [];
+    for (let b = 0; b < word1.length; b++) {
+        let letter = word1[b];
+        if (word2.includes(letter) && word3.includes(letter) && comprobante1.includes(letter) == false) {
+            array2.push(letter);
+            comprobante1.push(letter);
+        }
+    }
+}
+
+console.log(contar(repetidos));
+console.log(contar(array2));
